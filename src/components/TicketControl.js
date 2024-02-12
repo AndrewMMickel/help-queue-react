67be11a7832e4c60be4c82d444819c877c9d2dfd
AndrewMMicke <andrew.m.mickel@gmail.com>
2024-02-12 10:50:17 -0800

    add TicketDetail

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,12 +6,42 @@ class TicketControl extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            formVisibleOnPage: false,
+            mainTicketList: [],
+            selectedTicket: null
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            formVisibleOnPage: !prevState.formVisibleOnPage
+        }));
+    }
+
+    handleAddingNewTicketToList = (newTicket) => {
+        const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+        this.setState({
+            mainTicketList: newMainTicketList,
+            formVisibleOnPage: false
+        });
     }
 
     render() {
+        let currentlyVisibleState = null;
+        let buttonText = null;
+        if (this.state.formVisibleOnPage) {
+            currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+            buttonText = "Return to Ticket List";
+        } else {
+            currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />;
+            buttonText = "Add Ticket";
+        }
         return (
             <React.Fragment>
+                {currentlyVisibleState}
+                <button onClick={this.handleClick}>{buttonText}</button>
             </React.Fragment>
         );
     }
