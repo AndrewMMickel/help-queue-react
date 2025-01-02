a47dfdfa123b2783aa526cb7815d9e7d8f335698
AndrewMMicke <andrew.m.mickel@gmail.com>
2025-01-02 10:01:03 -0800

    add tests in both codes and reducers

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,4 +1,6 @@
 import ticketListReducer from '../../reducers/ticket-list-reducer';
+import { formatDistanceToNow } from 'date-fns';
+import * as c from './../actions/ActionTypes';
 
 describe('ticketListReducer', () => {
     let action;
@@ -6,6 +8,10 @@ describe('ticketListReducer', () => {
         names: 'Ryan & Aimen',
         location: '4b',
         issue: 'Redux action is not working correctly.',
+        timeOpen: new Date(),
+        formattedWaitTime: formatDistanceToNow(new Date(), {
+            addSuffix: true
+        }),
         id: 1
     };
 
@@ -58,6 +64,47 @@ describe('ticketListReducer', () => {
                 location: '2a',
                 issue: 'Reducer has side effects.',
                 id: 2
+            }
+        });
+    });
+
+    test('Should add a formatted wait time to ticket entry', () => {
+        const { names, location, issue, timeOpen, id } = ticketData;
+        action = {
+            type: c.UPDATE_TIME,
+            formattedWaitTime: '4 minutes ago',
+            id: id
+        };
+        expect(ticketListReducer({ [id]: ticketData }, action)).toEqual({
+            [id]: {
+                names: names,
+                location: location,
+                issue: issue,
+                timeOpen: timeOpen,
+                id: id,
+                formattedWaitTime: '4 minutes ago'
+            }
+        });
+    });
+    test('should successfully add a ticket to the ticket list that includes date-fns-formatted wait times', () => {
+        const { names, location, issue, timeOpen, formattedWaitTime, id } = ticketData;
+        action = {
+            type: c.ADD_TICKET,
+            names: names,
+            location: location,
+            issue: issue,
+            timeOpen: timeOpen,
+            formattedWaitTime: formattedWaitTime,
+            id: id
+        };
+        expect(ticketListReducer({}, action)).toEqual({
+            [id]: {
+                names: names,
+                location: location,
+                issue: issue,
+                timeOpen: timeOpen,
+                formattedWaitTime: 'less than a minute ago',
+                id: id
             }
         });
     });
